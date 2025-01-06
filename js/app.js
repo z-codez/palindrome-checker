87de75d579df8a113bf6554c4006c0d31f70754e
dci-student <137389454+z-codez@users.noreply.github.com>
2025-01-06 08:43:57 +0100

    Finished logic for the palindrome checker

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const checkButton = document.querySelector("#input-div button");
+const inputField = document.querySelector("#input-div input");
+const resultDiv = document.getElementById("result");
+
+// Event Listeners
+
+checkButton.addEventListener('click', checkPalindrome);
+
+
+function checkPalindrome() {
+    if (!inputField.value) {
+        alert("Please input a value to be checked!");
+        return;
+    }
+
+    const inputString = inputField.value;
+    //
+    let formattedString = inputString.toLowerCase()
+        // removes all non-alphanumeric characters and '_' character.
+        .replace(/\W|_/g, "");
+    console.log(formattedString);
+    let reversedString = "";
+
+    // Reversing the inputString
+    for (let i = formattedString.length - 1; i >= 0; i--) {
+        reversedString += formattedString.charAt(i);
+    }
+
+    if (formattedString === reversedString) {
+        resultDiv.innerText = inputString + " is a palindrome.";
+    } else {
+        resultDiv.innerText = inputString + " is not a palindrome.";
+    }
+}
